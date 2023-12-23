9711b286f210eb65b91f357383f5c254ca072542
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-23 20:43:47 +0800

    Update and rename sync_script.sh to sync.sh

---
+++ sync.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# 設定 Rclone 遠端和本地目錄
+REMOTE="$REMOTE_NAME:$REMOTE_PATH"
+LOCAL_DIR="$LOCAL_PATH"
+
+# 設定 inotify 監聽的事件
+EVENTS="create,modify,delete,move"
+
+# Telegram Bot 相關設定
+TELEGRAM_BOT_TOKEN="$BOT_TOKEN"
+TELEGRAM_CHAT_ID="$CHAT_ID"
+
+# 設定日誌文件
+LOG_FILE="/date/rclone_inotify.log"
+
+# 函數用於發送Telegram通知
+send_telegram_notification() {
+    local message="$1"
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=$message"
+}
+
+# 函數用於記錄日誌
+log_message() {
+    local message="$1"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$LOG_FILE"
+}
+
+# 安裝 rclone 如果不存在
+if ! command -v rclone &> /dev/null; then
+    log_message "錯誤：rclone 工具未安裝"
+    echo "正在安裝 rclone..."
+    curl https://rclone.org/install.sh | sudo bash
+    if [ $? -ne 0 ]; then
+        log_message "錯誤：rclone 安裝失敗"
+        exit 1
+    fi
+    log_message "rclone 安裝成功"
+fi
+
+# 安裝 inotify-tools 如果不存在
+if ! command -v inotifywait &> /dev/null; then
+    log_message "錯誤：inotify-tools 工具未安裝"
+    echo "正在安裝 inotify-tools..."
+    sudo apt-get install -y inotify-tools  # 使用 apt-get，需要根據你的系統進行調整
+    if [ $? -ne 0 ]; then
+        log_message "錯誤：inotify-tools 安裝失敗"
+        exit 1
+    fi
+    log_message "inotify-tools 安裝成功"
+fi
+
+# 檢查本地目錄是否存在
+if [ ! -d "$LOCAL_DIR" ]; then
+  log_message "錯誤：本地目錄 '$LOCAL_DIR' 不存在"
+  exit 1
+fi
+
+# 使用 inotifywait 監視目錄變化，當有事件發生時執行 Rclone 同步
+inotifywait -e "$EVENTS" -m -r --format '%w%f' "$LOCAL_DIR" |
+while read -r FILE
+do
+  # 檢查文件是否存在，以避免同步不存在的文件
+  if [ -e "$FILE" ]; then
+    rclone sync "$FILE" "$REMOTE"
+    log_message "文件同步完成: $FILE"
+    send_telegram_notification "文件同步完成: $FILE"
+  else
+    log_message "警告：文件 '$FILE' 不存在，無法同步"
+    send_telegram_notification "警告：文件 '$FILE' 不存在，無法同步"
+  fi
+done
