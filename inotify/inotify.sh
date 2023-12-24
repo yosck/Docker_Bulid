e10874a950f3872cce9cf70883c49b009bbf4f8e
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-24 21:04:40 +0800

    Update inotify.sh

--- inotify/inotify.sh
+++ inotify/inotify.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 使用環境變數
-LOCAL_DIRECTORY="${LOCAL_DIRECTORY:-/data/watch}"
-REMOTE_DIRECTORY="${REMOTE_DIRECTORY:-remote:dir}"
+WATCH_FOLDER="${WATCH_FOLDER:-/data/watch}"
+RCLONE_REMOTE="${RCLONE_REMOTE:-remote:dir}"
 
 # 安裝 inotify-tools（在Debian/Ubuntu系統上）
 apt-get update && apt-get install -y inotify-tools rclone
 
 # 監視目錄中的事件，一旦有變化，就同步到遠程目錄
-inotifywait -m -r -e create,modify,delete,move "$LOCAL_DIRECTORY" |
+inotifywait -m -r -e create,modify,delete,move "$WATCH_FOLDER" |
 while read path action file; do
     echo "File $file has been $action"
-    rclone sync "$LOCAL_DIRECTORY" "$REMOTE_DIRECTORY"
+    rclone sync "$WATCH_FOLDER" "$RCLONE_REMOTE"
 done
