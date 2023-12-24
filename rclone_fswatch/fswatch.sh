98538434d3f8d4d1c0ca0b9f4c77e6a62547593b
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-24 20:10:44 +0800

    Update fswatch.sh

--- rclone_fswatch/fswatch.sh
+++ rclone_fswatch/fswatch.sh
@@ -3,5 +3,5 @@
 # 監視本地目錄的變化，並使用 rclone 同步變化到遠端目錄
 fswatch -r "$LOCAL_PATH" \
 | while read -r change; do
-    rclone sync -vP "$LOCAL_PATH" "$REMOTE_PATH"
+    rclone sync -vP "$LOCAL_PATH" "$RCLONE_REMOTE"
   done
