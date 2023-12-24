558c1cffbe6f9dcf0f70c23f9324dbee8f8aafbe
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-24 19:44:07 +0800

    Update entr.sh

--- rclone_entr/entr.sh
+++ rclone_entr/entr.sh
@@ -2,8 +2,7 @@
 
 # 在腳本中使用環境變數
 echo "Using RCLONE_REMOTE: $RCLONE_REMOTE"
-echo "Using RCLONE_CONFIG: $RCLONE_CONFIG"
 echo "Using WATCH_FOLDER: $WATCH_FOLDER"
 
 # 最後，使用環境變數執行 rclone 命令，並將輸出附加到日誌文件
-find $WATCH_FOLDER | entr -r rclone sync -vP $WATCH_FOLDER $RCLONE_REMOTE
+find $WATCH_FOLDER | entr -rn rclone sync -vP $WATCH_FOLDER $RCLONE_REMOTE
