080dbd47175bc6686410196f8a3955d7c7f29a59
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-24 19:07:14 +0800

    Update entr.sh

--- rclone_entr/entr.sh
+++ rclone_entr/entr.sh
@@ -3,7 +3,7 @@
 # 在腳本中使用環境變數
 echo "Using RCLONE_REMOTE: $RCLONE_REMOTE"
 echo "Using RCLONE_CONFIG: $RCLONE_CONFIG"
-echo "Using WATCH_FOLDER: $$WATCH_FOLDER"
+echo "Using WATCH_FOLDER: $WATCH_FOLDER"
 
 
 # 最後，使用環境變數執行 rclone 命令
