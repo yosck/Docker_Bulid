5cdcd2dc8a28eecda16a8149e5a7f203928a6143
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-25 23:38:47 +0800

    Create sync.sh

---
+++ rclone_watchexec/sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 使用環境變數
+WATCH_DIR="${WATCH_DIR:-/data/watch}"
+RCLONE_REMOTE="${RCLONE_REMOTE:-remote:dir}"
+CONFIG_FILE="${CONFIG_FILE:-/data/rclone.conf}"
+
+# 執行 rclone 同步命令
+rclone sync --config="$CONFIG_FILE" -vP "$WATCH_DIR" "$RCLONE_REMOTE"
