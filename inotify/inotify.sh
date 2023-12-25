a2d1f528d7bc1467437da0ae0a1ed16cf519b752
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-25 23:37:01 +0800

    Update inotify.sh

--- inotify/inotify.sh
+++ inotify/inotify.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 使用環境變數
-WATCH_FOLDER="${WATCH_FOLDER:-/data/watch}"
+WATCH_DIR="${WATCH_DIR:-/data/watch}"
 RCLONE_REMOTE="${RCLONE_REMOTE:-remote:dir}"
 
 # 監視目錄中的事件，一旦有變化，就同步到遠程目錄
-inotifywait -m -r -e create,modify,delete,move "$WATCH_FOLDER" |
+inotifywait -m -r -e create,modify,delete,move "$WATCH_DIR" |
 while read path action file; do
     echo "File $file has been $action"
     # 執行 rclone.sh
