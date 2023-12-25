8fd4e8bbe9ee19eefe7ad98615500ec27f90e225
ann426 <71388764+ann426@users.noreply.github.com>
2023-12-25 23:37:09 +0800

    Update rclone.sh

--- inotify/rclone.sh
+++ inotify/rclone.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # 使用環境變數
-WATCH_FOLDER="${WATCH_FOLDER:-/data/watch}"
+WATCH_DIR="${WATCH_DIR:-/data/watch}"
 RCLONE_REMOTE="${RCLONE_REMOTE:-remote:dir}"
 CONFIG_FILE="${CONFIG_FILE:-/data/rclone.conf}"
 
 # rclone 同步
-rclone sync --config="$CONFIG_FILE" -vP "$WATCH_FOLDER" "$RCLONE_REMOTE"
+rclone sync --config="$CONFIG_FILE" -vP "$WATCH_DIR" "$RCLONE_REMOTE"
